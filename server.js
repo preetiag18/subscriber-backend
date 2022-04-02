47aa179a7d0f01d5e2bb97677fd7b980c6dcd951
preetiag18 <preetiag18@gmail.com>
2022-04-02 17:26:57 +0300

    subscriber backend code

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require("express")
+const { MongoClient, ServerApiVersion, MongoDBNamespace } = require('mongodb');
+const mongodb = require('mongodb');
+console.log("the password is" ,process.argv);
+
+const uri = `mongodb+srv://testUser:${process.argv[2]}@my-first-cluster.fkvhj.mongodb.net/?retryWrites=true&w=majority`
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+let db, users;
+client.connect(err => {
+    console.log("error is", err )
+    db = client.db("users")
+    users = db.collection("users")
+});
+
+const app = express();
+app.use(express.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+      )
+    next();
+});
+
+app.post("/add-user", (req, res) => {
+    console.log("body is", JSON.stringify(req.body));
+    users.insertOne(
+        { first_name: req.body.namee,
+        pincode : req.body.pincodee },
+         (err, result) => {
+             if (err) {
+                console.error(err)
+                res.status(500).json({ err: err })
+                return
+            }
+            console.log(result)
+            res.status(200).json(result.insertedId)
+         }
+    )
+})
+
+
+app.get("/showdetails", (req ,res) => {
+    const details = users.find({}).toArray(function (err, result) {
+      if (err) {
+        res.status(400).send("Error fetching listings!");
+     } else {
+         console.log("Results are " ,result);
+        res.json(result);
+      }
+    });
+    
+})
+
+app.delete('/:id',async (req, res) =>{
+
+    console.log(req.params.id);
+    
+    const result = await users.deleteOne({_id: new mongodb.ObjectId(req.params.id)});
+
+    //DELETE YOUR RECORD WITH YOUR PARAM.
+    res.send(result);
+})
+
+app.listen(8080, () => console.log("Node Js Server ready"))
